62c43dfb309f50b14acb2236fe747fca3296e05d
Yajnesh Kumar <113285428+YajneshKumar2004@users.noreply.github.com>
2024-11-26 16:49:48 +0530

    Create 0035. Search Insert Position.cpp

---
+++ 0035. Search Insert Position.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int index = 0;
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(nums[i] < target)
+            {
+                index++;
+            }
+            if(nums[i] == target)
+            {
+                return i;
+            }
+        }
+
+        return index;
+    }
+};
