a5b6ac0532fe1b91ead3e4d1797381b5e420a4f2
Yajnesh Kumar <113285428+YajneshKumar2004@users.noreply.github.com>
2024-11-27 10:41:33 +0530

    Update 0151. Reverse Words in a String.cpp

--- 0151. Reverse Words in a String.cpp
+++ 0151. Reverse Words in a String.cpp
@@ -23,3 +23,28 @@ public:
         return str;
     }
 };
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        istringstream stream(s);
+        string word;
+        vector<string>vec;
+        while(stream >> word)
+        {
+            vec.push_back(word);
+        }
+        string str = "";
+        for(int i=vec.size()-1; i>=0; i--)
+        {
+            str += vec[i];
+            if(i != 0)
+            {
+                str += ' ';
+            }
+        }
+        return str;
+    }
+};
